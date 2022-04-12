a84867b34e45ebb0b4d8f9ce6a1a7629161363e0
Martin Morales <martin.morales2356@gmail.com>
2022-04-11 22:25:14 -0300

    Correccion Firebase II

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { contexto } from './CartContext'
+import {addDoc, collection, serverTimestamp} from "firebase/firestore";
+import { db } from "./Firebase"
 
 const Carrito = () => {
 
     const {carrito,borrarDelCarrito, limpiarCarrito, calcularTotal} = useContext(contexto)
+    
+    const [compra , setCompra] = useState([])
+    let infoOrden = []
+    
+    const handleClick = () => {
+        
+        const orden = {
+            buyer : {
+                nombre : "Martin",
+                apellido : "Morales",
+                telefono:"2915233728",
+                email : "martin.morales2356@gmail.com"
+            },
+            items : carrito,
+            date : serverTimestamp(),
+            total : calcularTotal()
+        }
+        const ordenesCollection = collection(db,"ordenes")
+        const pedido = addDoc(ordenesCollection,orden)
+       
+        infoOrden = orden
+        pedido.then(res=>{console.log(compra)
+          setCompra({...infoOrden, id:res.id})})
+        
+      }
+      
+      console.log(compra)
 
     return (
         <>
@@ -18,6 +47,7 @@ const Carrito = () => {
                 <p>{item.cantidad}</p>
                 <button onClick={()=>borrarDelCarrito(item.producto)} className="quitarCarrito">Eliminar del carrito</button>
                 <button onClick={()=>limpiarCarrito()} className="quitarCarrito">Vaciar carrito</button>
+                <button onClick={handleClick} className="quitarCarrito">Confirmar compra</button>
                 </div>
             ))
             }
